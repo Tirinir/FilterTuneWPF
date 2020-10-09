22528f919da807bf13557335e5b673372115a5ed
Tirinir <deviuss@gmail.com>
2020-10-09 21:40:41 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FilterTuneWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectWorkingDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Open the file dialogue \n" +
+                "Let user choose a file \n" +
+                "Grab a list of similar files\n" +
+                "Populate the combobox \n" +
+                "Remember the directory");
+        }
+    }
+}
