572fec95fc5e99ad06e4b62634e4b9d32420b59e
Tirinir <deviuss@gmail.com>
2020-10-15 04:53:21 +0300

    Changed Templates from List to ObservableCollection to obtain INotifyPropertyChanged.
    "Save Template As" button now works (check for null required)
    
    Altered UI layout structure.

--- ViewModels/MainScreenViewModel.cs
+++ ViewModels/MainScreenViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Drawing;
 using System.Text;
 using System.Windows.Controls;
@@ -11,7 +12,7 @@ namespace FilterTuneWPF
     {
         private TemplateViewModel chosenTemplate;
 
-        public List<TemplateViewModel> Templates { get; set; }
+        public ObservableCollection<TemplateViewModel> Templates { get; set; }
         public TemplateViewModel ChosenTemplate
         {
             get => chosenTemplate; set
@@ -22,9 +23,10 @@ namespace FilterTuneWPF
         }
         public string testString { get; set; }
         public ICommand TestCommand { get; set; }
-        private List<TemplateViewModel> LoadTemplates(int numberOfTemplates)
+        public ICommand SaveTemplateCommand { get; set; }
+        private ObservableCollection<TemplateViewModel> LoadTemplates(int numberOfTemplates)
         {
-            Templates = new List<TemplateViewModel>();
+            Templates = new ObservableCollection<TemplateViewModel>();
             for (int i = 0; i < numberOfTemplates; i++)
             {
                 Templates.Add(new TemplateViewModel($"This is the {i}th selector", $"This is the {i}th parameter", $"Template {i}"));
@@ -33,19 +35,23 @@ namespace FilterTuneWPF
         }
         private void Test()
         {
-            object s = "Black";
             //System.Windows.Media.Color x = (System.Windows.Media.Color)s;
             testString = "Black";
             NotifyPropertyChanged("testString");
-        } 
-        
-        private List<TemplateViewModel> MockSavedTemplates { get; set; }
-        //Initializing variables for main screen
-        public MainScreenViewModel()
+        }
+        private ObservableCollection<TemplateViewModel> MockSavedTemplates { get; set; }
+        public void SaveTemplate() //update Templates with NewTemplateName value of chosenTemplate
         {
-            Templates = LoadTemplates(5);
+            Templates.Add(new TemplateViewModel(chosenTemplate.Selectors, chosenTemplate.Parameters, chosenTemplate.NewTemplateName));
+            NotifyPropertyChanged("Templates");
+        }
+        
+        public MainScreenViewModel() //Initializing variables for main screen
+        {
             TestCommand = new GenericCommand(x => Test());
+            Templates = LoadTemplates(5);
             MockSavedTemplates = LoadTemplates(5);
+            SaveTemplateCommand = new GenericCommand(x => SaveTemplate());
         }
     }
 }
--- ViewModels/TemplateViewModel.cs
+++ ViewModels/TemplateViewModel.cs
@@ -4,17 +4,19 @@ using System.Text;
 
 namespace FilterTuneWPF
 {
-    class TemplateViewModel : Notifier
+    class TemplateViewModel : Notifier // Templates and their metadata for presentation to the user
     {
         public TemplateViewModel(string selectors, string parameters, string templateName = "New template")
         {
             Selectors = selectors;
             Parameters = parameters;
             TemplateName = templateName;
+            NewTemplateName = templateName;
         }
         public bool Active { get; set; }
         public string TemplateName { get; set; }
         public string Selectors { get; set; }
         public string Parameters { get; set; }
+        public string NewTemplateName { get; set; }
     }
 }
